b094a29999622c1cb616c5936ac7887257ddb99f
Sufyan Sial <113730730+sufyansial112@users.noreply.github.com>
2023-10-31 18:43:08 +0000

    01project-bgChanger-final

--- 01Project-bgChanger/index.js
+++ 01Project-bgChanger/index.js
@@ -1,15 +1,27 @@
 const button = document.querySelectorAll(".button");
 const body = document.querySelector("body");
+const box = document.querySelector(".box");
 
 button.forEach((btn) => {
+  // const newli = document.createElement("button");
+  // newli.id = "pink";
+  // console.log(newli);
+
+  // box.appendChild(newli);
+  // console.log(box.children);
+
   console.log(btn);
   btn.addEventListener("click", function (e) {
     console.log(e);
     console.log(e.target);
     if (e.target.id === "green") {
-      body.style.backgroundColor = "green";
+      body.style.backgroundColor = e.target.id;
     } else if (e.target.id === "black") {
       body.style.backgroundColor = "black";
+    } else if (e.target.id === "brown") {
+      body.style.backgroundColor = "brown";
+    } else if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
     }
   });
 });
