4e995e360a51378f33effc24fdd143967fe11e38
Sufyan Sial <113730730+sufyansial112@users.noreply.github.com>
2023-11-01 18:30:43 +0000

    02project-BMI Calculator

---
+++ 02project-BMI Calculation/index.js
@@ -0,0 +1,8 @@
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = document.querySelector("#height").value;
+  const weight = document.querySelector("#weight").value;
+});
