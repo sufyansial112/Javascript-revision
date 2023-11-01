cf626a374d827a4a1d76a3a76341101c4033af18
Sufyan Sial <113730730+sufyansial112@users.noreply.github.com>
2023-11-01 18:55:50 +0000

    02Project-BMI Final

--- 02project-BMI Calculation/index.js
+++ 02project-BMI Calculation/index.js
@@ -3,6 +3,27 @@ const form = document.querySelector(".form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const height = document.querySelector("#height").value;
-  const weight = document.querySelector("#weight").value;
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+
+  const results = document.querySelector(".results");
+
+  if (height === "" || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please enter a valid height ${height}`;
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please enter a valid weight ${weight}`;
+  } else {
+    const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+    results.innerHTML = `${BMI}`;
+  }
+  const overWeight = document.querySelector(".over-weight");
+  const normalRange = document.querySelector(".normal-range");
+  const underWeight = document.querySelector(".under-weight");
+  if (results.innerHTML <= 19) {
+    underWeight.innerHTML = "You are underweight";
+  } else if (results.innerHTML > 19 && results.innerHTML < 25) {
+    normalRange.innerHTML = "You are in a normal range";
+  } else if (results.innerHTML >= 25) {
+    overWeight.innerHTML = "You are over weight";
+  }
 });
